489ea9996d7a6a32dbfb35a11a061c580f0d184f
gsgallant <Greg@Gregs-MacBook-Pro.local>
2016-02-24 13:39:03 -0500

    HW4 added

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -37,81 +37,90 @@ var crystal = {
 
 
 $(document).ready(function(){
+
 	
 	$('#red').append('<img src=' + crystal.red.pathtoimg + '>');
 	$('#red').click(function(){
+		if (endOfGame == false){
 		playerScore=playerScore + crystal.red.strength;
 		checkScore();
 		$('#playerScore').html(playerScore);
-		
+		};
 	})
 
 
 	$('#blue').append('<img src=' + crystal.blue.pathtoimg + '>');
 	$('#blue').click(function(){
+		if (endOfGame == false){
 		playerScore=playerScore + crystal.blue.strength;
 		checkScore();
 		$('#playerScore').html(playerScore);
+		};
 	})
 
 
 	$('#yellow').append('<img src=' + crystal.yellow.pathtoimg + '>');  
 	$('#yellow').click(function(){
+		if (endOfGame == false){
 		playerScore=playerScore + crystal.yellow.strength;
 		checkScore();
 		$('#playerScore').html(playerScore);
+		};
 	})
 
 
 	$('#green').append('<img src=' + crystal.green.pathtoimg + '>');
 	$('#green').click(function(){
+		if (endOfGame == false){
 		playerScore=playerScore + crystal.green.strength;
 		checkScore();
 		$('#playerScore').html(playerScore);
+		};
 	})
 
 
-function checkScore(){
-			$('#playerScore').html(playerScore);
-			if (playerScore == randomnum){
-					endOfGame = true;
-					wins++;
-					$('#announce').text("You Win!!");
-					}else if (playerScore > randomnum){
-						Playerloses = true;
+	function checkScore(){
+				$('#playerScore').html(playerScore);
+				if (playerScore == randomnum){
 						endOfGame = true;
-						losses++;
-						$('#announce').text("You Lose!");
-					}else{
-					// alert(playerScore);
-					console.log("playerScore: "+playerScore);
-					console.log("randomnumber: " + randomnum);
-					}
-					if (endOfGame){
-					$('#wins').html("Wins: " + wins);
-					$('#losses').html("Losses: "+losses);
-					setTimeout(startgame,2000);
-					}
-	//})
-}
+						wins++;
+						$('#announce').text("You Win!!");
+						}else if (playerScore > randomnum){
+							Playerloses = true;
+							endOfGame = true;
+							losses++;
+							$('#announce').text("You Lose!");
+						}else{
+						// alert(playerScore);
+						console.log("playerScore: "+playerScore);
+						console.log("randomnumber: " + randomnum);
+						}
+						if (endOfGame){
+						$('#wins').html("Wins: " + wins);
+						$('#losses').html("Losses: "+losses);
+						setTimeout(startgame,2500);
+						}
+	}
 
-function startgame(){
-		endOfGame = false;	
-		playLoses = false;
-		playerScore = 0
-		randomnum = Math.round(Math.random() * (120 - 19)) + 19;
-		
-		crystal.red.strength = Math.round(Math.random() * (12-1)) + 1;
-		crystal.blue.strength = Math.round(Math.random() * (12-1)) + 1;
-		crystal.yellow.strength = Math.round(Math.random() * (12-1)) + 1;
-		crystal.green.strength = Math.round(Math.random() * (12-1)) + 1;
-		
-		$('#playerScore').html(playerScore);
-		$('#randnum').html(randomnum);
-		$('#announce').text("New Game!");
-		$('#wins').html("Wins: " + wins);
-		$('#losses').html("Losses: "+losses);
-}
+	function startgame(){
+			endOfGame = false;	
+			playLoses = false;
+			playerScore = 0
+			randomnum = Math.round(Math.random() * (120 - 19)) + 19;
+			
+			crystal.red.strength = Math.round(Math.random() * (12-1)) + 1;
+			crystal.blue.strength = Math.round(Math.random() * (12-1)) + 1;
+			crystal.yellow.strength = Math.round(Math.random() * (12-1)) + 1;
+			crystal.green.strength = Math.round(Math.random() * (12-1)) + 1;
+			
+			$('#playerScore').html(playerScore);
+			$('#randnum').html(randomnum);
+			$('#announce').text("New Game!");
+			$('#wins').html("Wins: " + wins);
+			$('#losses').html("Losses: "+losses);
+	}
+
+
 
 startgame();
 
